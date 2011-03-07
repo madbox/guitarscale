aadbc1e27c681b4fd4bec402922c0edf2019d68a
madbox <madbox.mail@gmail.com>
2011-03-08 00:51:01 +0300

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GuitarChords::Application.config.secret_token = '177e5d5222af9a5e8a3f4085061c5e89f392c112d6240ef1ddf49df6b3f02f867a58a550e25f2d9346ed0516217b0db068767f278068f262fa900bdad3b3251b'
